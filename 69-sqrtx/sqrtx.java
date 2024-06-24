107a4087f45d58497927605b04583556b184e182
Leevesh Kumar <117984349+theleeveshkumar@users.noreply.github.com>
2024-06-24 09:19:10 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (71.77%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,7 +1,5 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x <= 1)
-            return x;
-        return (int)Math.floor(Math.sqrt(x));
+        return (int)Math.sqrt(x);
     }
 }
