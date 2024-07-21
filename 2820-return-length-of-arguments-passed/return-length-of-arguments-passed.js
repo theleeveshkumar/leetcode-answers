1185a762718ec26397f700e553efba78618c7d26
leevesh kumar <117984349+theleeveshkumar@users.noreply.github.com>
2024-07-21 18:15:09 +0530

    Time: 57 ms (24.88%) | Memory: 48.7 MB (58.69%) - LeetSync

--- 2820-return-length-of-arguments-passed/return-length-of-arguments-passed.js
+++ 2820-return-length-of-arguments-passed/return-length-of-arguments-passed.js
@@ -2,9 +2,7 @@
  * @param {...(null|boolean|number|string|Array|Object)} args
  * @return {number}
  */
-var argumentsLength = function(...args) {
-    return args.length;
-};
+const argumentsLength = (...args) => args.length;
 
 /**
  * argumentsLength(1, 2, 3); // 3
